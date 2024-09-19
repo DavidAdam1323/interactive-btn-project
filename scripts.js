7730f9192bb14cf183a8d5941c8d70a846daf3fa
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-19 14:36:38 +0100

    Toggle light bulb image and content background color on button click.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 function btnStatus() {
   let status = document.querySelector(".btn-text");
-  let bgContent = document.querySelector(".btn");
+  let bgContent = document.querySelector(".content");
+  let lightBulb = document.querySelector(".light-bulb");
 
   if (status.innerHTML === "OFF") {
     status.innerHTML = "ON";
-    bgContent.style.backgroundColor = "green";
+    bgContent.style.backgroundColor = "rgb(34, 34, 34";
+    lightBulb.src = "/images/bulb-on.png";
   } else {
     status.innerHTML = "OFF";
-    bgContent.style.backgroundColor = "red";
+    bgContent.style.backgroundColor = "rgb(9, 8, 6)";
+    lightBulb.src = "/images/bulb-off.png";
   }
 }
