59d43c3db8ffdecf4a97b8c1fda00c97a4d0a1bf
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-19 17:37:35 +0100

    Fix light bulb toggle logic and switch event handling.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,12 @@
 function btnStatus() {
-  let status = document.querySelector(".btn-text");
   let bgContent = document.querySelector(".content");
   let lightBulb = document.querySelector(".light-bulb");
+  let btnSlider = document.querySelector(".switch input"); 
 
-  if (status.innerHTML === "OFF") {
-    status.innerHTML = "ON";
-    bgContent.style.backgroundColor = "rgb(34, 34, 34";
+  if (btnSlider.checked) {
+    bgContent.style.backgroundColor = "rgb(34, 34, 34)";
     lightBulb.src = "/images/bulb-on.png";
   } else {
-    status.innerHTML = "OFF";
     bgContent.style.backgroundColor = "rgb(9, 8, 6)";
     lightBulb.src = "/images/bulb-off.png";
   }
